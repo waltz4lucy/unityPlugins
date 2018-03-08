a2bdaa22ff0f3729edda16e6dea6d1ebcc6bdc59
Lucy Diamond <waltz4lucy@gmail.com>
2018-03-08 10:57:37 +0900

    get mcc

---
+++ Assets/Scripts/Common/UnityNative.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+#region iOS
+
+public partial class UnityNative
+{
+#if !UNITY_EDITOR && UNITY_IPHONE
+    [DllImport("__Internal")]
+    static extern string IOSNativeGetMcc();
+#endif
+    public static string GetMcc()
+    {
+#if UNITY_EDITOR
+        return "450";
+#elif UNITY_ANDROID
+        return AndroidAction(javaClass =>
+        {
+            return javaClass.CallStatic<string>("GetMcc");
+        });
+#elif UNITY_IPHONE
+        return IOSNativeGetMcc();
+#endif
+    }
+}
+
+#endregion
+
+#region Android
+
+public partial class UnityNative
+{
+#if UNITY_ANDROID
+
+#endif
+}
+
+#endregion
---
+++ Assets/Scripts/Scenes/MCCSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MCCSceneManager : MonoBehaviour
+{
+    public Button button;
+    public Text text;
+
+    public void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Start()
+    {
+        button.onClick.AddListener(delegate {
+            OnClick();
+        });
+    }
+
+    public void OnClick()
+    {
+        text.text = UnityNative.GetMcc();
+    }
+}
